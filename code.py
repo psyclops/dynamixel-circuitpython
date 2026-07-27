c560c19104f29593bd5e4f60953f305f931daa2d
Nick Donaldson <psyclops@Holly-2.local>
2026-07-27 12:46:41 -0700

    CircuitPython's auto-run entry point

--- code.py
+++ code.py
@@ -5,12 +5,13 @@
 # -----------------------------------------------------------------------------
 #
 #
-# code.py -- CircuitPython's auto-run entry point. 
+# code.py -- CircuitPython's auto-run entry point
 #
-# demo.py performs all setup and calls an infinite loop so to run the program
-# a simple import is required here
+# demo.py performs all setup and calls an infinite loop to run the main program
+#
+# A simple import is all that is required here
 # 
-# Copy this file to the root of CIRCUITPYTHON folder together with demo.py or demo.mpy
+# Copy this file to the CIRCUITPYTHON folder together with demo.py or demo.mpy
 #
 
 import demo
